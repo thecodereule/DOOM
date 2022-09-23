e8e0a65530a2c258d83314920daa65f7060154c1
TehStud <igortadic.msc@gmail.com>
2022-09-23 21:21:12 +0200

    intialization: fixing bugs

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pygame as pg
 import sys
 from settings import *
 
-class Game: 
-	def __innit__(self):
+
+class Game:
+	def __init__(self):
 		pg.init()
 		self.screen = pg.display.set_mode(RES)
 		self.clock = pg.time.Clock()
@@ -14,4 +15,23 @@ class Game:
 	def update(self):
 		pg.display.flip()
 		self.clock.tick(FPS)
-		pg.display.set_caption(f'{self.clock.get_fps():.1f}')
+		pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+	def draw(self):
+		self.screen.fill('black')
+
+	def check_events(self):
+		for event in pg.event.get():
+			if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+				pg.quit()
+				sys.exit()
+
+	def run(self):
+		while True:
+			self.check_events()
+			self.update()
+			self.draw()
+
+if __name__ == '__main__':
+	game = Game()
+	game.run()
