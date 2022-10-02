3b05f9a59a808cd7d41e0ac2c11752f6e55ba9af
TehStud <igortadic.msc@gmail.com>
2022-10-03 00:11:52 +0200

    making an npc search for the player using a player search trigger

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ class Game:
 		self.screen = pg.display.set_mode(RES)
 		self.clock = pg.time.Clock()
 		self.delta_time = 1
+		self.global_trigger = False
+		self.global_event = pg.USEREVENT + 0
+		pg.time.set_timer(self.global_event, 40)
 		self.new_game()
 
 	def new_game(self):
@@ -42,7 +45,6 @@ class Game:
 		self.delta_time = self.clock.tick(FPS)
 		pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
-
 	def draw(self):
 		#self.screen.fill('black')
 		self.object_renderer.draw()
@@ -51,10 +53,13 @@ class Game:
 		#self.player.draw()
 
 	def check_events(self):
+		self.global_trigger = False
 		for event in pg.event.get():
 			if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
 				pg.quit()
 				sys.exit()
+			elif event.type == self.global_event:
+				self.global_trigger = True
 			self.player.single_fire_event(event)
 
 	def run(self):
--- npc.py
+++ npc.py
@@ -19,12 +19,40 @@ class NPC(AnimatedSprite):
         self.alive = True
         self.pain = False
         self.ray_cast_value = False
+        self.frame_counter = 0
+        self.player_search_trigger = False
 
     def update(self):
         self.check_animation_time()
         self.get_sprite()
         self.run_logic()
         #self.draw_ray_cast()
+
+    def check_wall(self, x, y):
+        return(x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx * self.size), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy * self.size)):
+            self.y += dy        
+
+
+    def movement(self):
+        next_pos = self.game.player.map_pos
+        next_x, next_y = next_pos
+        angle = math.atan2(next_y + 0.5 - self.y, next_x + 0.5 - self.x)
+        dx = math.cos(angle) * self.speed
+        dy = math.sin(angle) * self.speed
+        self.check_wall_collision(dx, dy)
+
+    def animate_death(self):
+        if not self.alive:
+            if self.game.global_trigger and self.animation_trigger and self.frame_counter < len(self.death_images) - 1:
+                self.death_images.rotate(-1)
+                self.image = self.death_images[0]
+                self.frame_counter += 1
+
 
     def animate_pain(self):
         self.animate(self.pain_images)
@@ -43,17 +71,30 @@ class NPC(AnimatedSprite):
     def check_health(self):
         if self.health < 1:
             self.alive = False
-            self.game.sound.npc_death
+            self.game.sound.npc_death.play()
 
 
     def run_logic(self):
         if self.alive:
             self.ray_cast_value = self.ray_cast_player_npc()
             self.check_hit_in_npc()
+
             if self.pain:
                 self.animate_pain()
+
+            elif self.ray_cast_value:
+                self.player_search_trigger = True
+                self.animate(self.walk_images)
+                self.movement()
+
+            elif self.player_search_trigger:
+                self.animate(self.walk_images)
+                self.movement()
+
             else:
                 self.animate(self.idle_images)
+        else:
+            self.animate_death()
 
 
     @property
